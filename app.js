8f8b2a2270b6b99820b7efd86515dc853f1b3793
Philip <philiplofters@gmail.com>
2018-07-16 12:42:08 +0000

    Added mlab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://Smoothpine:1apple@ds125555.mlab.com:25555/yelpcamp-db");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
